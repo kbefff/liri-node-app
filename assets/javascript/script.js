63492b02cfa042e75b7ebf4cf7b02bbd56383558
Coding <coding@Kelseys-MacBook-Pro.local>
2019-02-20 13:26:59 -0800

    inital setup

---
+++ assets/javascript/script.js
@@ -0,0 +1,22 @@
+/*JavaScript*/
+// iife - immediately invoked function expression
+// don't wait to execute a function, ececute this function upon my document being completely loaded
+// primarily used for games when something needs to appear imediately without you having to invoke it
+(function () {
+
+
+
+
+})();
+
+
+
+
+
+
+
+// jQuery
+$(document).ready(function () {
+
+
+});
